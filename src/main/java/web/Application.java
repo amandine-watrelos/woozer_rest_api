ef739b8465e14480ff1f71785a17b62ff991525e
Amandine Watrelos <amandine.watrelos@gmail.com>
2020-02-28 01:04:40 +0100

    [FEAT][GENERAL-33] ajout webservices upload/download de fichiers

--- src/main/java/web/Application.java
+++ src/main/java/web/Application.java
@@ -3,13 +3,16 @@ package web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import pojo.StorageProperties;
 
 @SpringBootApplication
 @ComponentScan({"service", "dao", "web"})
 @EntityScan("entity")
 @EnableJpaRepositories("dao")
+@EnableConfigurationProperties(StorageProperties.class)
 public class Application {
 
     public static void main(String[] args) {
---
+++ src/main/java/web/controller/FileController.java
@@ -0,0 +1,55 @@
+package web.controller;
+
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import service.StorageService;
+import web.dto.FileResponse;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+public class FileController {
+
+    private StorageService storageService;
+
+    public FileController(StorageService storageService) {
+        this.storageService = storageService;
+    }
+
+
+    @GetMapping("/download/{filename:.+}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String filename) {
+
+        Resource resource = storageService.loadAsResource(filename);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION,
+                        "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
+    }
+
+    @PostMapping("/upload-file")
+    public FileResponse uploadFile(@RequestParam("file") MultipartFile file) {
+        String name = storageService.store(file);
+
+        String uri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/download/")
+                .path(name)
+                .toUriString();
+
+        return new FileResponse(name, uri, file.getContentType(), file.getSize());
+    }
+
+    @PostMapping("/upload-multiple-files")
+    public List<FileResponse> uploadMultipleFiles(@RequestParam("files") MultipartFile[] files) {
+        return Arrays.stream(files)
+                .map(file -> uploadFile(file))
+                .collect(Collectors.toList());
+    }
+}
